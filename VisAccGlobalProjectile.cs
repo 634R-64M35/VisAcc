8cf51033714648afad4b608ceafe04069e104807
draedon gaming <lockout18yt@gmail.com>
2022-06-12 22:39:42 -0500

    Fishing stuff done

--- VisAccGlobalProjectile.cs
+++ VisAccGlobalProjectile.cs
@@ -1,77 +1,153 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace VisAcc {
-    public class VisAccGlobalProjectile : GlobalProjectile {
-        public override bool InstancePerEntity => true;
+	public class VisAccGlobalProjectile : GlobalProjectile {
+		public override bool InstancePerEntity
+			=> true;
 
-        public static readonly Color[] PossibleLineColors = new Color[] {
-            new Color(255, 215, 0), // A gold color
+		public static readonly Color[] PossibleLineColors = new Color[] {
+			new Color(255, 215, 0), // A gold color
 			new Color(0, 191, 255) // A blue color
 		};
 
-        private int fishingLineColorIndex;
+		public override void OnSpawn(Projectile projectile, IEntitySource source) {
+			VisAccPlayer modPlayer = Main.LocalPlayer.GetModPlayer<VisAccPlayer>();
+			if (modPlayer.lavaProof && projectile.bobber) {
+				modPlayer.lavaProof = false;
+			}
+			modPlayer.highTest = false;
+		}
 
-        private Color FishingLineColor => PossibleLineColors[fishingLineColorIndex];
+		public override bool PreDraw(Projectile projectile, ref Color drawColor)
+		{
+			VisAccPlayer modPlayer = Main.LocalPlayer.GetModPlayer<VisAccPlayer>();
+			if (modPlayer.lavaProof && projectile.bobber) {
+				Main.instance.LoadProjectile(projectile.type);
+				Texture2D hook = ModContent.Request<Texture2D>("VisAcc/Textures/LavaProofFishingHook").Value;
+				Vector2 drawOrigin = new(hook.Width / 2, hook.Height / 2);
+				Main.EntitySpriteDraw(hook, projectile.position - Main.screenPosition + new Vector2(15, 7), hook.Bounds, drawColor, projectile.rotation,
+					drawOrigin, projectile.scale, SpriteEffects.None, 0);
+				return false;
+			}
+			return true;
+		}
 
-        public override void OnSpawn(Projectile projectile, IEntitySource source) {
-            VisAccPlayer modPlayer = Main.LocalPlayer.GetModPlayer<VisAccPlayer>();
-            if (modPlayer.lavaProof &&
-                (projectile.type == ProjectileID.BobberBloody
-                || projectile.type == ProjectileID.BobberFiberglass
-                || projectile.type == ProjectileID.BobberFisherOfSouls
-                || projectile.type == ProjectileID.BobberFleshcatcher
-                || projectile.type == ProjectileID.BobberGolden
-                || projectile.type == ProjectileID.BobberHotline
-                || projectile.type == ProjectileID.BobberMechanics
-                || projectile.type == ProjectileID.BobberReinforced
-                || projectile.type == ProjectileID.BobberScarab
-                || projectile.type == ProjectileID.BobbersittingDuck
-                || projectile.type == ProjectileID.BobberWooden)) {
-                modPlayer.lavaProof = false;
-                modPlayer.highTest = false;
-            }
-            // Decide color of the pole by getting the index of a random entry from the PossibleLineColors array.
-            fishingLineColorIndex = (byte)Main.rand.Next(PossibleLineColors.Length);
-        }
+		public override bool PreDrawExtras(Projectile projectile) {
+			VisAccPlayer modPlayer = Main.LocalPlayer.GetModPlayer<VisAccPlayer>();
+			if (modPlayer.highTest == true)
+			{
+				const int XPositionAdditive = 45;
+				const float YPositionAdditive = 35f;
 
-        public override bool PreDraw(Projectile projectile, ref Color drawColor) {
-            VisAccPlayer modPlayer = Main.LocalPlayer.GetModPlayer<VisAccPlayer>();
-            if (modPlayer.lavaProof &&
-                (projectile.type == ProjectileID.BobberBloody
-                || projectile.type == ProjectileID.BobberFiberglass
-                || projectile.type == ProjectileID.BobberFisherOfSouls
-                || projectile.type == ProjectileID.BobberFleshcatcher
-                || projectile.type == ProjectileID.BobberGolden
-                || projectile.type == ProjectileID.BobberHotline
-                || projectile.type == ProjectileID.BobberMechanics
-                || projectile.type == ProjectileID.BobberReinforced
-                || projectile.type == ProjectileID.BobberScarab
-                || projectile.type == ProjectileID.BobbersittingDuck
-                || projectile.type == ProjectileID.BobberWooden)) {
-                Main.instance.LoadProjectile(projectile.type);
-                Texture2D hook = ModContent.Request<Texture2D>("VisAcc/Textures/LavaProofFishingHook").Value;
-                Vector2 drawOrigin = new(hook.Width / 2, hook.Height / 2);
-                Main.EntitySpriteDraw(hook, projectile.position - Main.screenPosition + new Vector2(15, 7), hook.Bounds, drawColor, projectile.rotation, 
-                    drawOrigin, projectile.scale, SpriteEffects.None, 0);
-                return false;
-            }
-            return true;
-        }
-    }
-    /*public class VisAccFishingLine : ModProjectile
-    {
-        public override void ModifyFishingLine(ref Vector2 lineOriginOffset, ref Color lineColor)
-        {
-            // Change these two values in order to change the origin of where the line is being drawn.
-            // This will make it draw 47 pixels right and 31 pixels up from the player's center, while they are looking right and in normal gravity.
-            lineOriginOffset = new Vector2(47, -31);
-            // Sets the fishing line's color. Note that this will be overridden by the colored string accessories.
-            lineColor = FishingLineColor;
-        }
-    }*/
+
+				Player player = Main.player[projectile.owner];
+
+				if (!projectile.bobber || player.inventory[player.selectedItem].holdStyle <= 0)
+					return false;
+
+				Vector2 lineOrigin = player.MountedCenter;
+				lineOrigin.Y += player.gfxOffY;
+				int type = player.inventory[player.selectedItem].type;
+
+				// This variable is used to account for Gravitation Potions
+				float gravity = player.gravDir;
+
+				lineOrigin.X += XPositionAdditive * player.direction;
+
+				if (player.direction < 0)
+					lineOrigin.X -= 13f;
+			
+				lineOrigin.Y -= YPositionAdditive * gravity;
+
+				if (gravity == -1f)
+					lineOrigin.Y -= 12f;
+
+				// RotatedRelativePoint adjusts lineOrigin to account for player rotation.
+				lineOrigin = player.RotatedRelativePoint(lineOrigin + new Vector2(8f), true) - new Vector2(8f);
+				Vector2 playerToProjectile = projectile.Center - lineOrigin;
+				bool canDraw = true;
+
+				if (playerToProjectile.X == 0f && playerToProjectile.Y == 0f)
+					return false;
+
+				float playerToProjectileMagnitude = playerToProjectile.Length();
+				playerToProjectileMagnitude = 12f / playerToProjectileMagnitude;
+				playerToProjectile *= playerToProjectileMagnitude;
+				lineOrigin -= playerToProjectile;
+				playerToProjectile = projectile.Center - lineOrigin;
+
+				// This math draws the line, while allowing the line to sag.
+				while (canDraw) {
+					float height = 12f;
+					float positionMagnitude = playerToProjectile.Length();
+
+					if (float.IsNaN(positionMagnitude) || float.IsNaN(positionMagnitude))
+						break;
+
+					if (positionMagnitude < 20f) {
+						height = positionMagnitude - 8f;
+						canDraw = false;
+					}
+
+					playerToProjectile *= 12f / positionMagnitude;
+					lineOrigin += playerToProjectile;
+					playerToProjectile.X = projectile.position.X + projectile.width * 0.5f - lineOrigin.X;
+					playerToProjectile.Y = projectile.position.Y + projectile.height * 0.1f - lineOrigin.Y;
+
+					if (positionMagnitude > 12f) {
+						float positionInverseMultiplier = 0.3f;
+						float absVelocitySum = Math.Abs(projectile.velocity.X) + Math.Abs(projectile.velocity.Y);
+
+						absVelocitySum = Math.Min(absVelocitySum, 16f);
+						absVelocitySum = 1f - absVelocitySum / 16f;
+						positionInverseMultiplier *= absVelocitySum;
+						absVelocitySum = positionMagnitude / 80f;
+						absVelocitySum = Math.Min(absVelocitySum, 1f);
+
+						positionInverseMultiplier *= absVelocitySum;
+						positionInverseMultiplier = Math.Max(positionInverseMultiplier, 0f);
+
+						absVelocitySum = 1f - projectile.localAI[0] / 100f;
+						positionInverseMultiplier *= absVelocitySum;
+
+						if (playerToProjectile.Y > 0f) {
+							playerToProjectile.Y *= 1f + positionInverseMultiplier;
+							playerToProjectile.X *= 1f - positionInverseMultiplier;
+						} else {
+							absVelocitySum = Math.Abs(projectile.velocity.X) / 3f;
+							absVelocitySum = Math.Min(absVelocitySum, 1f);
+							absVelocitySum -= 0.5f;
+							positionInverseMultiplier *= absVelocitySum;
+
+							if (positionInverseMultiplier > 0f) {
+								positionInverseMultiplier *= 2f;
+							}
+
+							playerToProjectile.Y *= 1f + positionInverseMultiplier;
+							playerToProjectile.X *= 1f - positionInverseMultiplier;
+						}
+
+						var texture = TextureAssets.FishingLine.Value;
+						var linePos = new Vector2(lineOrigin.X - Main.screenPosition.X + texture.Width * 0.5f + 1, lineOrigin.Y - Main.screenPosition.Y + texture.Height * 0.5f + 1);
+						var sourceRectangle = new Rectangle(0, 0, texture.Width, (int)height);
+						float rotation = playerToProjectile.ToRotation() - MathHelper.PiOver2;
+						var origin = new Vector2(texture.Width * 0.5f, 0f);
+
+						Main.EntitySpriteDraw(texture, linePos + new Vector2(2, 2), sourceRectangle, Color.DarkGray, rotation, origin, 1f, SpriteEffects.None, 0);
+						Main.EntitySpriteDraw(texture, linePos, sourceRectangle, Color.DimGray, rotation, origin, 1f, SpriteEffects.None, 0);
+						Main.EntitySpriteDraw(texture, linePos - new Vector2(2, 2), sourceRectangle, Color.DarkGray, rotation, origin, 1f, SpriteEffects.None, 0);
+					}
+				}
+				return false;
+			}
+			return true;
+		}
+	}
 }
